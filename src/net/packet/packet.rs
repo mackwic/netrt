d29824ff54e3786ba33f8745511f57b107d61e98
Thomas Wickham <thomas@wickham.epimeros.org>
2016-01-13 23:02:19 +0100

    add some packet structure

---
+++ src/net/packet/packet.rs
@@ -0,0 +1,115 @@
+/*
+   This Source Code Form is subject to the terms of the Mozilla Public
+   License, v. 2.0. If a copy of the MPL was not distributed with this
+   file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
+use net::packet::size::*;
+
+#[derive(Debug, PartialEq, Eq)]
+struct PacketDescriptor(header::PacketType, IdSizeType);
+
+mod header {
+
+    use std::convert::{From, Into};
+    //use net::packet::size::*;
+
+    /// internal representation of PacketHeader. Use the accessors
+    #[derive(Debug, PartialEq, Eq)]
+    struct PacketHeader(u64);
+    /// internal repsentation. Use the methods
+    #[derive(Debug, PartialEq, Eq)]
+    struct AckBitField(u64);
+
+    #[derive(Debug, PartialEq, Eq)]
+    pub enum PacketType {
+        HeartBeat, Data
+    }
+    const MASK_TYPE : u8 = 0b1000_0000;
+
+    impl<'a> From<&'a u8> for PacketType {
+        fn from(other: &'a u8) -> PacketType {
+            match other & MASK_TYPE {
+                MASK_TYPE => PacketType::Data,
+                _ => PacketType::HeartBeat
+            }
+        }
+    }
+    impl Into<PacketType> for u8 {
+        fn into(self) -> PacketType {
+            match self & MASK_TYPE {
+                MASK_TYPE => PacketType::Data,
+                _ => PacketType::HeartBeat
+            }
+        }
+    }
+
+    #[test]
+    fn equivalence_packet_type() {
+        let vals = [
+            0b1100_0000, 0b1100_0001, 0b1101_0000, 0b1111_1111,
+            0b0000_0000, 0b0000_1111, 0b1011_1111, 0b1010_1010
+        ];
+
+        for v in vals.iter() {
+            let t1 = PacketType::from(v);
+            let t2 : PacketType = v.into();
+            assert_eq!(t1, t2);
+        }
+    }
+
+    impl PacketHeader {
+        pub fn header(&self) -> super::PacketDescriptor {
+            let first_byte : u8 = (self.0 >> 56) as u8;
+            super::PacketDescriptor(first_byte.into(), first_byte.into())
+        }
+
+        pub fn ack_bitfield(&self) -> AckBitField {
+            AckBitField((self.0 << 8) & 0xFFFF_FFFF_FFFF_FF00)
+        }
+    }
+
+    #[test]
+    fn header() {
+        use net::packet::size::IdSizeType;
+        use super::PacketDescriptor;
+
+        let tmp = PacketHeader(0xC000_0000_0000_0000);
+        assert_eq!(PacketDescriptor(PacketType::Data, IdSizeType::Short), tmp.header());
+        let tmp = PacketHeader(0x8000_0000_0000_0000);
+        assert_eq!(PacketDescriptor(PacketType::Data, IdSizeType::Long), tmp.header());
+        let tmp = PacketHeader(0x4000_0000_0000_0000);
+        assert_eq!(PacketDescriptor(PacketType::HeartBeat, IdSizeType::Short), tmp.header());
+        let tmp = PacketHeader(0);
+        assert_eq!(PacketDescriptor(PacketType::HeartBeat, IdSizeType::Long), tmp.header());
+    }
+
+    #[test]
+    fn ack_bitfield() {
+        let tmp = PacketHeader(0xdead_beef_b00b_cafe);
+        assert_eq!(AckBitField(0xad_beef_b00b_cafe_00), tmp.ack_bitfield());
+    }
+
+}
+
+mod body {
+    use net::packet::size::*;
+
+    type RawData = [u8];
+
+    #[derive(Debug, PartialEq, Eq)]
+    struct DataChunkHeader<Size : IdSize> {
+        size: u16,
+        number: u16,
+        head: Size,
+        data: RawData
+    }
+
+    #[derive(Debug)]
+    enum PacketBody<Size : IdSize> {
+        HeartBeat,
+        Data(RawData),
+        DataChunk(DataChunkHeader<Size>)
+    }
+
+}
---
+++ src/net/packet/size.rs
@@ -0,0 +1,92 @@
+/*
+   This Source Code Form is subject to the terms of the Mozilla Public
+   License, v. 2.0. If a copy of the MPL was not distributed with this
+   file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
+//use std::cmp::*;
+use std::convert::{From, Into};
+
+pub const LONG_SIZE : u8 = 64;
+pub const SHORT_SIZE : u8 = 32;
+pub const MASK_SIZE : u8 = 0b0100_0000;
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum IdSizeType { Long, Short }
+pub type LongSize = u64;
+pub type ShortSize = u32;
+
+pub trait IdSize {
+    fn size(&self) -> u8;
+    fn is_long(&self) -> bool { self.size() == LONG_SIZE }
+    fn is_short(&self) -> bool { self.size() == SHORT_SIZE }
+}
+
+impl IdSize for LongSize { fn size(&self) -> u8 { LONG_SIZE } }
+impl IdSize for ShortSize { fn size(&self) -> u8 { SHORT_SIZE } }
+
+impl IdSizeType {
+    pub fn make_new(&self) -> Box<IdSize> {
+        match self {
+            &IdSizeType::Long => Box::new(0u64 as LongSize),
+            &IdSizeType::Short => Box::new(0u32 as ShortSize)
+        }
+    }
+}
+
+impl<'a> From<&'a u8> for IdSizeType {
+    fn from(other: &'a u8) -> IdSizeType {
+        match other & MASK_SIZE {
+            MASK_SIZE => IdSizeType::Short,
+            _ => IdSizeType::Long
+        }
+    }
+}
+
+/*impl From<u8> for IdSizeType {
+    fn from(other: u8) -> IdSizeType { IdSizeType::from(&other) }
+}*/
+
+impl Into<IdSizeType> for u8 {
+    fn into(self) -> IdSizeType {
+        match self & MASK_SIZE {
+            MASK_SIZE => IdSizeType::Short,
+            _ => IdSizeType::Long
+        }
+    }
+}
+
+#[test]
+fn test_conversion() {
+    let short_vals = [0b0100_0000, 0b0100_0001, 0b0101_0000, 0b1111_1111];
+    let long_vals = [0b0000_0000, 0b0000_1111, 0b1011_1111, 0b1010_1010];
+    let tests = [(IdSizeType::Short, &short_vals), (IdSizeType::Long, &long_vals)];
+
+    for &(ref expected, ref values) in tests.into_iter() {
+        for &v in values.into_iter() { assert_eq!(*expected, IdSizeType::from(&v)) }
+    }
+}
+
+#[test]
+fn equivalence() {
+    let vals = [
+        0b0100_0000, 0b0100_0001, 0b0101_0000, 0b1111_1111,
+        0b0000_0000, 0b0000_1111, 0b1011_1111, 0b1010_1010
+    ];
+
+    for v in vals.iter() {
+        let t1 = IdSizeType::from(v);
+        let t2 : IdSizeType = v.into();
+        assert_eq!(t1, t2);
+    }
+}
+
+#[test]
+fn test_is_long() {
+    let tmp = IdSizeType::Long;
+    assert_eq!(true, tmp.make_new().is_long());
+    assert_eq!(false, tmp.make_new().is_short());
+    let tmp = IdSizeType::Short;
+    assert_eq!(false, tmp.make_new().is_long());
+    assert_eq!(true, tmp.make_new().is_short())
+}
